9da7cd1aa159e8c2e2ddc5b4ceb1f44e3ffbe44e
Janani M <janani.01.jan.07@gmail.com>
2026-03-25 18:41:01 +0530

    CharacterDigitSpecialSymbolCount.java

---
+++ Day6/CharacterDigitSpecialSymbolCount.java
@@ -0,0 +1,25 @@
+class Solution {
+    public static void main(String[] args) {
+        String str = "Hello##123";
+
+        int alphabet = 0;
+        int digit = 0;
+        int special = 0;
+
+        for (int i = 0; i < str.length(); i++) {
+            char ch = str.charAt(i);
+
+            if ((ch >= 'a' && ch <= 'z') || (ch >= 'A' && ch <= 'Z')) {
+                alphabet++;
+            } else if (ch >= '0' && ch <= '9') {
+                digit++;
+            } else {
+                special++;
+            }
+        }
+
+        System.out.println("alphabet: " + alphabet);
+        System.out.println("digit: " + digit);
+        System.out.println("special: " + special);
+    }
+}
