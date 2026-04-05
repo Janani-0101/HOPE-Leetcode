42accadcc9780e456054bd5f23680d227bd7eafa
Janani M <janani.01.jan.07@gmail.com>
2026-04-05 15:56:50 +0530

    Car_Racing_Game.java
    Removed unnecessary comments and whitespace for cleaner code.

---
+++ Car_Racing_Game.java
@@ -0,0 +1,97 @@
+abstract class Vehicle {
+    protected String name;
+    protected int speed;
+    protected int fuel;
+    public Vehicle(String name, int speed, int fuel) {
+        this.name = name;
+        this.speed = speed;
+        this.fuel = fuel;
+    }
+    public abstract void accelerate();
+    public abstract void brake();
+    public void showStatus() {
+        System.out.println(name + " Speed: " + speed + " Fuel: " + fuel);
+    }
+}
+class Car extends Vehicle {
+
+    public Car(String name, int speed, int fuel) {
+        super(name, speed, fuel);
+    }
+    public void accelerate() {
+        speed += 10;
+        fuel -= 5;
+    }
+    public void brake() {
+        speed -= 5;
+    }
+}
+
+// Inheritance + Method Overriding
+class SportsCar extends Car {
+
+    public SportsCar(String name, int speed, int fuel) {
+        super(name, speed, fuel);
+    }
+
+    @Override
+    public void accelerate() {
+        speed += 20;
+        fuel -= 10;
+    }
+}
+
+// Interface
+interface RaceActions {
+    void startRace();
+    void endRace();
+}
+class Player {
+    private String playerName;
+    private Vehicle car;
+    public Player(String playerName, Vehicle car) {
+        this.playerName = playerName;
+        this.car = car;
+    }
+    public String getPlayerName() {
+        return playerName;
+    }
+    public Vehicle getCar() {
+        return car;
+    }
+}
+class RaceGame implements RaceActions {
+    private Player[] players;
+    public RaceGame(Player[] players) {
+        this.players = players;
+    }
+    public void startRace() {
+        System.out.println("Race Started!");
+
+        for (Player p : players) {
+            p.getCar().accelerate();  
+            p.getCar().showStatus();
+        }
+    }
+    public void endRace() {
+        System.out.println("Race Finished!");
+    }
+}
+public class Main {
+    public static void main(String[] args) {
+
+        Vehicle car1 = new Car("Normal Car", 50, 100);
+        Vehicle car2 = new SportsCar("Ferrari", 70, 120);
+
+        Player p1 = new Player("Player1", car1);
+        Player p2 = new Player("Player2", car2);
+
+        Player[] players = {p1, p2};
+
+        RaceGame game = new RaceGame(players);
+
+        game.startRace();
+        game.endRace();
+    }
+  }
+}
